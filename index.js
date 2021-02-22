e907c90e72744a5fd363f77cde77e2274f5332a4
김범현 <bumhyunkim@newin.co.kr>
2021-02-22 14:12:10 +0900

    Fix

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const sharp = require("sharp");
 
 const s3 = new AWS.S3();
 
+const transforms = [
+  { name: "thumb", width: 470 },
+  { name: "content", width: 950 },
+];
+
 exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name;
   const Key = event.Records[0].s3.object.key;
@@ -12,14 +17,22 @@ exports.handler = async (event, context, callback) => {
 
   try {
     const s3Object = await s3.getObject({Bucket, Key}).promise();
+    
+    await Promise.all(
+      transforms.map(async (item) => {
+        const resizedImage = await sharp(s3Object.Body).resize({ width: item.width }).toFormat(requiredFormat).toBuffer();
+        
+        await s3.putObject({
+          Bucket,
+          Key: `${item.name}/${filename}`,
+          Body: resizedImage,
+          ContentType: "image"
+        }).promise();
+        
+        return callback(null, resizedImage.length);
+      })
+    )
 
-    const resizedImage = await sharp(s3Object.Body).resize(200, 200, { fit: "inside" }).toFormat(requiredFormat).toBuffer();
-    await s3.putObject({
-      Bucket,
-      Key: `thumb/${filename}`,
-      Body: resizedImage
-    }).promise();
-    return callback(null, resizedImage.length);
   } catch (error) {
     return callback(error);
   }
